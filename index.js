7dab1762c54cf7412ada60538b161bd42ed77af8
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-14 17:58:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+const theBeatlesPlay = (musicians, instruments) => {
+  
+const newArray = []
+
+  for (let i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i])
+  }  
+  
+  return newArray
+}
+
+function johnLennonFacts() {
+  var newArray = [];
+  var facts = [ "foo", "bar" ];
+  var i = 0;
+  while (i < facts.length) {
+    newArray.push(facts[i] + "!!!");
+   i++;
+  }
+  return newArray;
+}
